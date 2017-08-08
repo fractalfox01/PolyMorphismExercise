16bd5f9855790ff550660b7bedd0b636c3b762b2
FractalFox <flgtom@aol.com>
2017-08-08 12:04:41 -0700

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,18 @@
+/*
+We are going to go back to the car analogy.
+Create a base class called car
+it should have a few fields that would be appropriate for a generic car class.
+engine, cylinders, wheels, etc.
+Constructors should initialize cylinders (number of) and name, and set wheels to 4
+and engine to true. cylinders and names would be passed parameters.
+
+Create some methods like startEngine, accelerate, and brake
+
+show a message for each in the base class
+Now create 3 sub classes for your favorite vehicles.
+Override the appropriate methods to demonstrate polymorphism in use.
+put all classes in the one java file (this one).
+ */
 class Car {
     private String name;
     private boolean engine;
@@ -32,11 +47,21 @@ class Car {
     public int getDoors() {
         return doors;
     }
+
+    public void startEngine(){
+        System.out.println("Engine on " + name + " is being started");
+    }
+    public void accelerate(){
+        System.out.println(name + " is accelerating");
+    }
+    public void brake(){
+        System.out.println(name + " is braking");
+    }
 }
 
 class Taurus extends Car{
     public Taurus(){
-        super("Taurus", true, 6,4);
+        super("Taurus", false, 6,4);
     }
 
     @Override
@@ -54,11 +79,21 @@ class Ram1500 extends Car{
     public String getName() {
         return "Ram1500";
     }
+    @Override
+    public void startEngine(){
+        if(!getEngine()){
+            System.out.println("oops, No engine in " + getName());
+            System.out.println("Engine is " + getEngine());
+        }
+        else{
+            System.out.println("Starting engine on " + getName());
+        }
+    }
 }
 
 class Jeep extends Car{
     public Jeep(){
-        super("Vehicle", true, 6,2);
+        super("Jeep", true, 6,2);
     }
 
     @Override
@@ -68,17 +103,22 @@ class Jeep extends Car{
 }
 public class Main {
     public static void main(String[] args) {
-        Car mycar = new Car("tom",true,0,1);
+
 
         Car taurus = new Taurus();
         System.out.println(taurus.getName());
+        taurus.startEngine();
 
         System.out.println();
 
         Car jeep = new Jeep();
         System.out.println(jeep.getName());
-        System.out.println(jeep.getDoors());
-        System.out.println(jeep.getName().toUpperCase());
+        jeep.startEngine();
 
+        System.out.println();
+
+        Car ram = new Ram1500();
+        System.out.println(ram.getName());
+        ram.startEngine();
     }
 }
