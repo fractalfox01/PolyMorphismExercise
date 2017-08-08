26393d31cd777a27f273bb6980636f91a5a2e09f
fractalfox01 <flgtom@aol.com>
2017-08-08 11:36:51 -0700

    updated 11:36am

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 class Car {
     private String name;
-    private String engine;
+    private boolean engine;
     private int cylinders;
     private int wheels;
     private int doors;
 
-    public Car(String name, String engine, int cylinders, int wheels, int doors){
+    public Car(String name, boolean engine, int cylinders, int doors){
         this.name = name;
         this.engine = engine;
         this.cylinders = cylinders;
-        this.wheels = wheels;
+        this.wheels = 4;
         this.doors = doors;
     }
 
@@ -17,7 +17,7 @@ class Car {
         return name;
     }
 
-    public String getEngine() {
+    public boolean getEngine() {
         return engine;
     }
 
@@ -34,10 +34,51 @@ class Car {
     }
 }
 
+class Taurus extends Car{
+    public Taurus(){
+        super("Taurus", true, 6,4);
+    }
+
+    @Override
+    public String getName() {
+        return "Taurus";
+    }
+}
+
+class Ram1500 extends Car{
+    public Ram1500(){
+        super("Ram1500", true,8,2);
+    }
+
+    @Override
+    public String getName() {
+        return "Ram1500";
+    }
+}
+
+class Jeep extends Car{
+    public Jeep(){
+        super("Vehicle", true, 6,2);
+    }
+
+    @Override
+    public String getName() {
+        return "Jeep";
+    }
+}
 public class Main {
     public static void main(String[] args) {
-        Car mycar = new Car("tom","stuff",0,1,2);
+        Car mycar = new Car("tom",true,0,1);
 
+        Car taurus = new Taurus();
+        System.out.println(taurus.getName());
+
+        System.out.println();
+
+        Car jeep = new Jeep();
+        System.out.println(jeep.getName());
+        System.out.println(jeep.getDoors());
+        System.out.println(jeep.getName().toUpperCase());
 
     }
 }
